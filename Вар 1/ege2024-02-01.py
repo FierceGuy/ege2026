287a20a95a77ce97e3f7b2e9751d2c607ab14c44
Серафим <fierceguy13@gmail.com>
2025-02-24 10:57:06 +0300

    ege 2024

--- Вар 1/ege2024-02-01.py
+++ Вар 1/ege2024-02-01.py
@@ -1,7 +1,7 @@
- print ('z w y x')
- for x in range(2):
-     for y in range(2):
-         for z in range(2):
-             for w in range(2):
-                 if (((w <= y) <= x) or not(z))==0:
-                     print(z, w , y ,x)
+print ('z w y x')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (((w <= y) <= x) or not(z))==0:
+                    print(z, w , y ,x)
