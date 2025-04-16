35fd482aedc1b4b25c4cf823cd7bb512a72c4a4a
Серафим <fierceguy13@gmail.com>
2025-04-16 19:54:13 +0300

    №14

---
+++ ege2025-14-04.py
@@ -0,0 +1,18 @@
+def convert(number, system):
+    result = ""
+    while number != 0:
+        result += str(number % system)
+        number = number // system
+    return result[::-1]
+
+max0 = 0
+
+cool = 7**400 + 7**300
+for x in range(7**400, 0, -1):
+    res = cool - x
+    res = convert(res, 7)
+    lol =  res.count('0')
+    if lol > max0:
+        max0 = lol
+    print (lol)
+    break
--- ege2026-14-03.py
+++ ege2026-14-03.py
@@ -1,23 +1,21 @@
 # Значение арифметического выражения 7400 + 7300 – х, где х – натуральное число, не превышающее 7400, 
 # записали в системе счисления с основанием 7. Определите наибольшее количество нулей, которое может содержать 
 # семиричная запись значения данного арифметического выражения.
-
 def convert(number, system):
     result = ""
     while number != 0:
-        result += number % system
+        result += str(number % system)
         number = number // system
     return result[::-1]
 
 max0 = 0
 
-cool = 7400 + 7300
-for x in range(7400, 0, -1):
+cool = 7**400 + 7**300
+for x in range(7**400, 0, -1):
     res = cool - x
     res = convert(res, 7)
     lol =  res.count('0')
     if lol > max0:
         max0 = lol
-    print (max0)
-
-
+    print (lol)
+    break
