b744cee90f3e360a4d56345411366dfabd10162e
Серафим <fierceguy13@gmail.com>
2025-10-01 09:00:20 +0300

    ща решать будем

--- ege2024-05-02.py
+++ ege2024-05-02.py
@@ -3,4 +3,14 @@ def f(N):
     for _ in range(2):
         sumd = R.count("1")
         R = R + str(sumd%2)
-    
+    return int(R, 2)
+
+
+
+Rlist = []
+for N in range (40):
+    R = (f(N))
+    if R > 123:
+        Rlist.append(f(N))
+
+print(min(Rlist))
