2954060cd1b75a8fa4581be0b6a3d6e33589337a
Серафим <fierceguy13@gmail.com>
2025-10-01 09:13:37 +0300

    srtfh

--- ege2024-05-02.py
+++ ege2024-05-02.py
@@ -1,3 +1,4 @@
+
 def f(N):
     R = bin (N)[2:]
     for _ in range(2):
@@ -5,10 +6,8 @@ def f(N):
         R = R + str(sumd%2)
     return int(R, 2)
 
-
-
 Rlist = []
-for N in range (40):
+for N in range (100):
     R = (f(N))
     if R > 123:
         Rlist.append(f(N))
