bcb5c7b75fc5c360924a128de465b1c30c636f62
Серафим <fierceguy13@gmail.com>
2025-10-15 09:55:09 +0300

    сука

--- Вар 1/ege2024-08-01.py
+++ Вар 1/ege2024-08-01.py
@@ -1,1 +1,23 @@
-alphabet = "0123456789AB"
+# alphabet = "0123456789ABCDEF"
+
+def convert(number, system):
+    result =  []
+    while number > 0:
+        result.append(number % system)
+        number = number // system
+    return result[::-1]
+
+def test(number):
+    d1 = number.count(7)
+    d911 =  number.count(9) +  number.count(10) +  number.count(11)
+    
+    if d1 == 1 and d911 <= 3:
+        return True
+    return False
+
+count = 0
+for n in range(12**5):
+    if test(convert(n,2)):
+        count+=1
+
+print(count)
