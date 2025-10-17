a36597413eab51adf43aa64bc0e7e4526ae3aeaa
Серафим <fierceguy13@gmail.com>
2025-10-17 11:58:47 +0300

    udi

--- Вар 1/ege2024-08-01.py
+++ Вар 1/ege2024-08-01.py
@@ -8,16 +8,17 @@ def convert(number, system):
     return result[::-1]
 
 def test(number):
+
     d1 = number.count(7)
     d911 =  number.count(9) +  number.count(10) +  number.count(11)
-    
-    if d1 == 1 and d911 <= 3:
+
+    if d1 == 1 and d911 < 3:
         return True
     return False
 
 count = 0
 for n in range(12**5):
-    if test(convert(n,2)):
+    if test(convert(n,12)):
         count+=1
 
 print(count)
